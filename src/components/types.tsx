4a2585212713731145a46f76234415485e051013
Carlos ALvarez <carlosmail1978@gmail.com>
2023-04-24 20:41:57 +0200

    Modify structure of the components

---
+++ src/components/types.tsx
@@ -0,0 +1,9 @@
+export interface gentlemenStructure {
+  id: number;
+  name: string;
+  profession: string;
+  twitter: string;
+  picture: string;
+  alternativeText: string;
+  selected: boolean;
+}
